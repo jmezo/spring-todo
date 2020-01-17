a46c82763bbd25df4c51324984a3e888c0470334
Robert Boros <robbie13.boros@gmail.com>
2020-01-17 16:47:11 +0100

    implement ResourceServerConfigurer

--- src/main/java/hu/flowacademy/todo/config/WebSecurityConfig.java
+++ src/main/java/hu/flowacademy/todo/config/WebSecurityConfig.java
@@ -7,45 +7,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @EnableWebSecurity
 @EnableResourceServer
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ResourceServerConfigurer {
 
-  @Autowired
-  private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-  @Autowired
-  private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
-  @Autowired
-  public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
-      auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-  }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 
-  @Override
-  @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
-    // @formatter:off
-		http.authorizeRequests()
-		.antMatchers("/", "/login", "/oauth/**", "/oauth/token/revokeById/**", "/tokens/**").permitAll()
-        .antMatchers("/api/users", "/api/users/**").hasAnyAuthority(Role.ADMIN.name())
-		.anyRequest().authenticated()
-		.and().formLogin().permitAll()
-		.and().csrf().disable();
-		// @formatter:on
-  }
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {}
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/oauth/**", "/oauth/token/revokeById/**", "/tokens/**").permitAll()
+                .antMatchers("/api/users", "/api/users/**").hasAnyAuthority(Role.ADMIN.name())
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().csrf().disable();
+        // @formatter:on
+    }
 
 }
